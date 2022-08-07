5172569dbcce20e0bd4d0096e3eda1cb2a9980a5
MinhajSadik <MinhajoSadik@gmail.com>
2022-08-07 16:31:39 +0600

    [WORKING] on going and now updating API

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
@@ -13,7 +12,7 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.json({ limit: "30mb", extended: true }));
 
 const PORT = process.env.PORT || 5000;
