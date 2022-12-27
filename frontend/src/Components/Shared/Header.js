bff712615fa8bea09ca1ac6b17c2ae14f240c777
MinhajSadik <MinhajoSadik@gmail.com>
2022-12-27 08:26:43 +0600

    [DEPLOYING] trying to deploy on vercel

--- frontend/src/Components/Shared/Header.js
+++ frontend/src/Components/Shared/Header.js
@@ -48,80 +48,82 @@ const Header = () => {
   };
 
   return (
-    <MDBNavbar fixed="top" expand="lg" style={{ backgroundColor: "#f0e6ea" }}>
-      <MDBContainer>
-        <MDBNavbarBrand
-          href="/"
-          style={{ color: "black", fontWeight: "600", fontSize: "22px" }}
-        >
-          Blog Website
-        </MDBNavbarBrand>
-        <MDBNavbarToggler
-          type="button"
-          aria-expanded="false"
-          aria-label="Toogle navigation"
-          onClick={() => setShow(!show)}
-          style={{ color: "black" }}
-        >
-          <MDBIcon icon="bars" fas />
-        </MDBNavbarToggler>
-        <MDBCollapse show={show} navbar>
-          <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
-            {user?.result?._id && (
-              <h5 style={{ marginRight: "30px", marginTop: "27px" }}>
-                Logged in as: {user?.result?.name}
-              </h5>
-            )}
-            <MDBNavbarItem>
-              <MDBNavbarLink href="/">
-                <p className="header-text">Home</p>
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            {user?.result?._id && (
-              <>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href="/addPost">
-                    <p className="header-text">Add New Post</p>
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href="/dashboard">
-                    <p className="header-text">Dashboard</p>
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-              </>
-            )}
-            {user?.result?._id ? (
+    <div>
+      <MDBNavbar fixed="top" expand="lg" style={{ backgroundColor: "#f0e6ea" }}>
+        <MDBContainer>
+          <MDBNavbarBrand
+            href="/"
+            style={{ color: "black", fontWeight: "600", fontSize: "22px" }}
+          >
+            Blog Website
+          </MDBNavbarBrand>
+          <MDBNavbarToggler
+            type="button"
+            aria-expanded="false"
+            aria-label="Toogle navigation"
+            onClick={() => setShow(!show)}
+            style={{ color: "black" }}
+          >
+            <MDBIcon icon="bars" fas />
+          </MDBNavbarToggler>
+          <MDBCollapse show={show} navbar>
+            <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+              {user?.result?._id && (
+                <h5 style={{ marginRight: "30px", marginTop: "27px" }}>
+                  Logged in as: {user?.result?.name}
+                </h5>
+              )}
               <MDBNavbarItem>
-                <MDBNavbarLink href="/login">
-                  <p className="header-text" onClick={() => handleLogout()}>
-                    Logout
-                  </p>
+                <MDBNavbarLink href="/">
+                  <p className="header-text">Home</p>
                 </MDBNavbarLink>
               </MDBNavbarItem>
-            ) : (
-              <MDBNavbarItem>
-                <MDBNavbarLink href="/login">
-                  <p className="header-text">Login</p>
-                </MDBNavbarLink>
-              </MDBNavbarItem>
-            )}
-          </MDBNavbarNav>
-          <form className="d-flex input-group w-auto" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search Post"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <div style={{ marginTop: "5px", marginLeft: "5px" }}>
-              <MDBIcon fas icon="search" />
-            </div>
-          </form>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+              {user?.result?._id && (
+                <>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/addPost">
+                      <p className="header-text">Add New Post</p>
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                  <MDBNavbarItem>
+                    <MDBNavbarLink href="/dashboard">
+                      <p className="header-text">Dashboard</p>
+                    </MDBNavbarLink>
+                  </MDBNavbarItem>
+                </>
+              )}
+              {user?.result?._id ? (
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/login">
+                    <p className="header-text" onClick={() => handleLogout()}>
+                      Logout
+                    </p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              ) : (
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/login">
+                    <p className="header-text">Login</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              )}
+            </MDBNavbarNav>
+            <form className="d-flex input-group w-auto" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search Post"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <div style={{ marginTop: "5px", marginLeft: "5px" }}>
+                <MDBIcon fas icon="search" />
+              </div>
+            </form>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </div>
   );
 };
 
