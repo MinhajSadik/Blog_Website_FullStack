54c490229f3a58f936447dc4921db85e6dfb687e
MinhajSadik <MinhajoSadik@gmail.com>
2022-12-27 08:52:34 +0600

    [DEPLOYED] done on render

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,14 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 
 const DB_URL_LOCAL = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
 
 const DB_URL_REMOTE = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu4qt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
